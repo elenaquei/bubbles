cd720427a40f7231e28884957fe5247b6a3cc0c1
Elena <elenaquei@hotmail.it>
2021-05-18 20:40:29 -0400

    numerical solution does not converge

--- code ODE + simplices/continuation_equation_simplex.m
+++ code ODE + simplices/continuation_equation_simplex.m
@@ -5,10 +5,12 @@ U = null(DF_x);
 
 lin_coef_vec1 = [ U(:,1).', - dot(U(:,1).', Xi_vec2vec(x1))];
 new_eq = F_new.scalar_equations.number_equations_lin+1;
-F_new = change_lin_coef_vector(F_new.scalar_equations, lin_coef_vec1, new_eq); 
+F_new.scalar_equations = change_lin_coef_vector(F_new.scalar_equations, ...
+    lin_coef_vec1, new_eq); 
 
 
 lin_coef_vec2 = [ U(:,2).', - dot(U(:,2).', Xi_vec2vec(x1))];
 new_eq = F_new.scalar_equations.number_equations_lin+1;
-F_new = change_lin_coef_vector(F_new.scalar_equations, lin_coef_vec2, new_eq); 
+F_new.scalar_equations = change_lin_coef_vector(F_new.scalar_equations, ...
+    lin_coef_vec2, new_eq); 
 end
--- code ODE + simplices/template_FHN.m
+++ code ODE + simplices/template_FHN.m
@@ -16,9 +16,9 @@ end
 
 % problem dependent
 nu = 1.05;
-n_nodes = 50;
+n_nodes = 10;
 step_size = 10^-4;
-n_iter =5;
+n_iter =10;
 save_file = 'saved elements/FHN_simplex_validation'; % path where the validation will be saved
 
 % starting parameters
@@ -31,13 +31,13 @@ epsilon_fix = 0.2480;
 % construct the numerical solution with forward integration from known
 % initial conditions
 init_coord  = [0.5667, 0.4864];
-approx_period = 14.85;
+approx_period = 14.7;
 
 % right hand side
 f=@(t,x)[x(1)*(x(1)-alpha)*(1-x(1))-x(2)+I_fix;epsilon_fix*( x(1)-x(2) )];
 
 % forward integration
-[tout, yout] = ode45(f,linspace(0,approx_period,n_nodes*2),init_coord);
+[tout, yout] = ode23s(f,linspace(0,approx_period,n_nodes*20),init_coord);
 
 
 % transformation to Xi_vector from time series
@@ -48,7 +48,7 @@ x_sol = time_series2Xi_vec(tout,yout,n_nodes);
 
 % ?- \dot x1 + x1^2 - x1 \alpha - x1^3- \alphau x1 - x2 + I\n - \dot x2 + \epsilon x1 - \epsilon \gamma x2
 %
-string_FHN = '- dot x1 + l1 x1^2 - alpha l1 x1 -l1  x1^3 - alpha l1 x1^2 - l1 x2 + I \n - dot x2 + epsilon l1 x1 - epsilon l1 x2'; % general FHN
+string_FHN = '- dot x1 + l1 x1^2 - alpha l1 x1 - l1 x1^3 + alpha l1 x1^2 - l1 x2 + I \n - dot x2 + epsilon l1 x1 - epsilon l1 x2'; % general FHN
 % plug in all the variables
 string_FHN_vars = strrep(string_FHN, 'alpha' , num2str(alpha)); 
 % fixed point vector field
@@ -61,24 +61,19 @@ string_FHN_vars = strrep(string_FHN_vars, 'l1' , '');
 string_FHN_vars = strrep(string_FHN_vars, 'I' , 'l1'); 
 string_FHN_vars = strrep(string_FHN_vars, 'epsilon' , 'l2');
 
+x_sol.vector(:,1:n_nodes-4) = 0;
+x_sol.vector(:,n_nodes+5:end) = 0;
+x_sol = symmetrise(x_sol);
 
 % constructing the ODEs systems
 % fixed pho
+% scal_eq = fancy_scalar_condition(x_sol);
 scal_eq = default_scalar_eq(x_sol);
 polynomial_fix = from_string_to_polynomial_coef(string_FHN_fix);
 F_fix = full_problem(scal_eq, polynomial_fix);
-bool_Hopf = 0;
-if bool_Hopf
-    % adds two new scalar equations
-    F_fix = F_update_Hopf(F_fix,x_sol);
-else
-    F_fix.scalar_equations = fancy_scalar_condition(x_sol, F_fix.scalar_equations);
-end
-% F_fix = continuation_equation_simplex(F_fix,x_sol);
-
 
 % refining the approximation with Newton method
-x_sol = Newton_2(x_sol,F_fix,30,10^-7);
+x_sol_N = Newton_2(x_sol,F_fix,30,10^-7);
 
 polynomial = from_string_to_polynomial_coef(string_FHN_vars);
 big_Hopf = Taylor_series_Hopf(polynomial,n_nodes);
@@ -88,11 +83,16 @@ big_Hopf = Taylor_series_Hopf(polynomial,n_nodes);
 % scalar = [omega, I, epsilon, a, x], vector = (y-x)/a
 x = mean(yout,1);
 amplitude = norm(yout - x);
-moved_vector = x_sol.vector;
+moved_vector = x_sol_N.vector;
 moved_vector(:,n_nodes+1) = moved_vector(:,n_nodes+1) - x.';
 vector = moved_vector/amplitude;
-sol = Xi_vector([x_sol.scalar,I_fix, epsilon_fix, amplitude, x], vector);
+sol = Xi_vector([x_sol_N.scalar,I_fix, epsilon_fix, amplitude, x], vector);
 
+big_Hopf = F_update_Hopf(big_Hopf,sol);
+
+% test_Hopf = big_Hopf;
+% test_Hopf = continuation_equation_simplex(test_Hopf,sol);
+% sol_N = Newton_2(sol,test_Hopf,30,10^-7);
 
 % launch the validation
 bool_Hopf = 1;
