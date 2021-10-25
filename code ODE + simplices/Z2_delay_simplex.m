72e77af007bf7f6364e2125494a4a88d9c2f2af3
ElenaTUM <elenaquei@hotmail.it>
2021-10-25 18:22:34 +0200

    Z2 bounds now include the second derivative wr.t. psi
    note: the derivative of the scalar function inputed by the user is not included!

--- code ODE + simplices/Z2_delay_simplex.m
+++ code ODE + simplices/Z2_delay_simplex.m
@@ -18,7 +18,7 @@ D3F2_int = three_intval(D3F20, D3F21, D3F22);
 
 block_norm_A = block_norm(M_int, P_int, Q_int, R_int, D3F2_int/(modes+1), phi_int, x0);
 
-upper_bound_psi_DF = [1,2,3]; %TODO
+upper_bound_psi_DF = dpsiDF(alpha, x, Rmax);
 
 Z22 = block_norm_A * upper_bound_psi_DF;
 
@@ -35,7 +35,14 @@ Z21 = block_norm_AM * upper_bound_DDF_without_dpsi;
 Z2 = Z22 + Z21;
 end
 
-function upper_bound_psi_DF = dpsiDF(alpha, x, Rmax)
+function DpsiDF = dpsiDF(alpha, x, Rmax)
+% MOSTLY DONE
+modes = x.nodes;
+if nu^(2*modes+2) < exp(1)
+    Lemma_bound = nu^(2*modes+2)/ (exp(1) * log(nu^(2*modes+2)));
+else
+    Lemma_bound = 1;
+end
 
 K = -x.nodes:x.nodes;
 x_norm = norm(x) + Rmax;
@@ -57,120 +64,90 @@ KKv_norm = KKx_norm(x.size_scalar+1:end);
 DpsiDF = zeros(length(x),1);
 
 for i=1:alpha.vector_field.n_equations % equation
+    DpsiDF_i = 0;
     for j=1:alpha.vector_field.n_terms(i) % element of the equation
-        d=[alpha.vector_field.power_scalar{i}(:,j).', alpha.vector_field.power_vector{i}{j}.'];
+        power_loc=[alpha.vector_field.power_scalar{i}(:,j).', alpha.vector_field.power_vector{i}{j}.'];
         const=abs(alpha.vector_field.value{i}(j));
-        N=length(x);
-        e=eye(N);
+        delay_loc = alpha.vector_field.delay{i}{j};
         
-        % DDH_i = sum_{k=1,d_k>0}^N sum_{j=1,(d-e_k)_n>0}^N
-        % (d-e_k)_n d_k x^{d-e_k-e_n} * dir1^e_k * dir2^e_n
+        X = prod(x_norm.^power_loc);
+        V_delay = prod(x_norm(delay_loc));
         
-        % special case: second derivative w.r.t. psi
-        for k = 1
-            power = d - e(:,1) - e(:,1);
-            if any(power<0)
-                continue
-            end
-            has_delay = (alpha.delay{i}{j}~=0); 
-            delay = alpha.delay{i}{j};
-            delay_v_norm = norm_v.^has_delay;
-            temp_dpsiDF_firsterm = const*d(1)*(d(1)-1) * x_norm(1)^(d(1)-2) * prod(x_norm(2:end).^power(2:end))*prod(v_norm.*has_delay);
-            delay_derivative=0;
-            for term = 1:x.size_vector
-                delay_derivative = delay_derivative + delay(term)*Kv_norm(term)*prod(v_norm(1:term-1).*has_delay(1:term-1))*prod(v_norm(term+1:end).*has_delay(term+1:end));
-            end
-            temp_dpsiDF_secondterm =const*d(1)* x_norm(1)^(d(1)-1) * prod(x_norm(2:end).^power(2:end))*delay_derivative;
-            temp_dpsiDF_thirdterm = const * prod(x_norm.^power) 
-        end
-        for k=2:length(x)
-            %if d(k)<=0
-            %    continue
-            %end
-            % second derivative, not needed here for n=1:length(x)
-                %if d(n)-e(n,k)<=0
-                %    continue
-                %end
-                power=d-e(k,:);
-                power_scalar=power(1:size_scal);
-                power_vector=power(size_scal+1:end);
-                if any(power<0)
-                    continue
+        if any(delay_loc)
+            if power_loc(1)>0
+                delay_term1 = 0;
+                for l = find(delay_loc)
+                    delay_term1 = delay_term1 + abs(delay_loc(l))/v_norm(l) * ( Kv_norm(l) + KKv_norm(l));
                 end
-                if k<=size_scal
-                    temp_dir1=dir1.scalar(k);%prod(dir1.scalar.^(e(k,1:size_scal)));
-                else
-                    temp_dir1=dir1_conv(k-size_scal,:);%product_FFT(dir1_conv,e(k,size_scal+1:end));
+                delay_term1 = power_loc(1)/x_norm(1) *delay_term1;
+                delay_term2 = 0;
+            end
+            for l1 = find(delay_loc)
+                for l2 = find(delay_loc)
+                    if l1 == l2
+                        continue
+                    end
+                    delay_term2 = delay_term2 + abs(delay_loc(l1))/v_norm(l1) *  Kv_norm(l1) * abs(delay_loc(l2))/v_norm(12) *  Kv_norm(l2) ;
                 end
-                has_delay = (alpha.delay{i}{j}(k)~=0); 
-                derivative_delay = prod(Kx.^has_delay);
-                temp_DDF(i,:)= temp_DDF(i,:) + const*d(k)*...
-                    prod(lambda_norm.^(power_scalar.'))*...
-                    prod(v_norm.^(power_vector.')).*...
-                    derivative_delay;
             end
         end
+        
+        non_delay_term = 0;
+        delay_term3 = 0;
+        for k = 1:length(x)
+            non_delay_term = non_delay_term + d(k) /x_norm(k) *(d(1) - k_is_1)/x_norm(1);
+            for l = find(delay_loc)
+                delay_term3 = delay_term3 +  d(k) /x_norm(k) * Kv_norm(l) * abs(delay_loc(l))/v_norm(l);
+            end
+        end
+        if all(delay_loc == 0)
+            DpsiDF_ij = const * X * V_delay * non_delay_term;
+            DpsiDF_i = DpsiDF_i + DpsiDF_ij;
+            continue
+        end
+        delay_term4 = 0;
+        for k = find(delay_loc)
+            delay_term5 = 0;
+            for l = find(delay_loc)
+                delay_term5 = delay_term5 + Kv_norm(l) * delay_loc(l)/v_norm(l);
+            end
+            delay_term4 = delay_term4 + 1/ v_norm(k) * (power_loc(1)/x_norm(1) + Lemma_bound * delay_loc(k) + delay_term5);;
+        end
+        DpsiDF_ij = const * X * V_delay * ( delay_term1 + delay_term2 + delay_term3 + delay_term4 + non_delay_term);
+        DpsiDF_i = DpsiDF_i + DpsiDF_ij;
     end
+    DpsiDF(i+x.size_scalar) = DpsiDF_i;
 end
 
+% second derivative of G(x) = 0
 
-% second derivative of G(x)
-temp_DDG=zeros(alpha.scalar_equations.num_equations,length(x_conv));
-if isintval(temp_DDF)
-    temp_DDG = intval(temp_DDG);
-elseif isintval(alpha.scalar_equations.polynomial_equations.value(1))
-    temp_DDF = intval(temp_DDF);
-    temp_DDG = intval(temp_DDG);
-end
-
-
+% second derivative of the scalar polynomial equations
 for i=1:alpha.scalar_equations.number_equations_pol % equation
     % alpha.scalar_equations.number_equations_lin+
+    DpsiDF_i = 0;
     for j=1:alpha.scalar_equations.polynomial_equations.n_terms(i) % element of the equation
         d=[alpha.scalar_equations.polynomial_equations.power_scalar{i}(:,j).', alpha.scalar_equations.polynomial_equations.power_vector{i}{j}.'];
         const=alpha.scalar_equations.polynomial_equations.value{i}(j);
         N=length(d);
         e=eye(N);
         
-        % DDH_i = sum_{k=1,d_k>0}^N sum_{j=1,(d-e_k)_n>0}^N
-        % (d-e_k)_n d_k x^{d-e_k-e_n} * dir1^e_k * dir2^e_n
+        X = prod(x_norm.^power_loc);
         
-        for k=1:N
-            %if d(k)<=0
-            %    continue
-            %end
-            for n=1:N
-                %if d(n)-e(n,k)<=0
-                %    continue
-                %end
-                power=((d-e(k,:)-e(n,:)).');
-                power_scalar=power(1:size_scal);
-                power_vector=power(size_scal+1:end);
-                if any(power<0)
-                    continue
-                end
-                if k<=size_scal
-                    temp_dir1=dir1.scalar(k);%prod(dir1.scalar.^(e(k,1:size_scal)));
-                else
-                    temp_dir1=dir1_conv(k-size_scal,:);%product_FFT(dir1_conv,e(k,size_scal+1:end));
-                end
-                if n<=size_scal
-                    temp_dir2=dir2.scalar(n);%prod(dir2.scalar.^(e(n,1:size_scal)));
-                else
-                    temp_dir2=dir2_conv(n-size_scal,:);%product_FFT(dir2_conv,e(n,size_scal+1:end));
-                end
-                temp_DDG(alpha.scalar_equations.number_equations_lin+i,:)= ...
-                    temp_DDG(alpha.scalar_equations.number_equations_lin+i,:) + const*d(k)*(d(n)-e(k,n))*...
-                    prod(xBar.scalar.^(power_scalar.'))*...
-                    product_FFT(x_conv,power_vector).*...
-                    temp_dir1.*...
-                    temp_dir2;
-            end
+        non_delay_term = 0;
+        for k = 1:length(x)
+            non_delay_term = non_delay_term + d(k) /x_norm(k) *(d(1) - k_is_1)/x_norm(1);
         end
+        DpsiDF_ij = const * X * V_delay * non_delay_term;
+        DpsiDF_i = DpsiDF_i + DpsiDF_ij;
     end
+    DpsiDF(i+alpha.scalar_equations.num_equations) = DpsiDF_i;
 end
 
+% MISSING: USER INPUT DERIVATIVE!!
+warning('User input derivative not included yet')
+end
 
+function upper_bound_DDF_without_psi(alpha, x, Rmax)
 end
 
 function block = block_norm(M, P, Q, R, D3F2, psi, x)
@@ -188,12 +165,6 @@ norm_V3 = 1 / abs(psi);
 
 block = [ norm_M + norm_V1 + norm_V3, norm_P
     norm_Q+norm_V2, n_R];
-end
-
-function upper_bound_psi_DF()
-end
-
-function upper_bound_DDF_without_psi()
 end
 
 function n = norm_C_to_ell1(P, nodes, size_scalar, size_vector)
