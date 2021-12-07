94cfe7630785b746f8dd792da1f02bc06212e8e9
Elena <elenaquei@hotmail.it>
2021-12-07 18:32:43 +0100

    better - even better - really amazing - complex rotation back to real

--- code ODE + simplices/core_code/continuation_equation_simplex.m
+++ code ODE + simplices/core_code/continuation_equation_simplex.m
@@ -29,7 +29,7 @@ F_new.scalar_equations = change_lin_coef_vector(F_new.scalar_equations, ...
 end
 
 
-function rotated_coefs = good_rotation(coefs, x0)
+function rotated_coefs = good_rotation_unused(coefs, x0)
 rotated_coefs = 0*coefs;
 for i = 1:size(coefs,2)
     x_vec = coefs(:,i);
@@ -42,6 +42,102 @@ for i = 1:size(coefs,2)
         warning('The rotation might not have been sufficient')
     end
     rotated_coefs(:,i) = rotated_x_vec;
+end
+
+end
+
+
+function rotated_coefs = good_rotation(coefs, x0)
+
+first_rotation = ones(1,8);
+x = coefs(1:x0.size_scalar,1);
+y = coefs(1:x0.size_scalar,2);
+conj_prod = @(x,y) dot(x,y);
+
+A_first = [imag(x), real(x), imag(y), real(y)];
+f_first = @(c) [A_first*c;
+    dot(c(1)+1i*c(2),c(1)+1i*c(2)) + dot(c(3)+1i*c(4),c(3)+1i*c(4)) - 1];
+df_first = @(c) [A_first
+    2*c(1:4).'];
+
+first_rescaling = newton(first_rotation(1:4).', f_first, df_first);
+
+coefs_rescaled = 0*coefs;
+coefs_rescaled(:,1) = (first_rescaling(1)+1i*first_rescaling(2)) * coefs(:,1) + ...
+     (first_rescaling(3)+1i*first_rescaling(4)) * coefs(:,2);
+if abs(first_rescaling(1)+1i*first_rescaling(2)) > (first_rescaling(3)+1i*first_rescaling(4))
+    coefs_rescaled(:,2) = coefs(:,2);
+else
+    coefs_rescaled(:,2) = coefs(:,1);
+end
+
+% Gram-Schmidt reduction on coefs_rescaled
+proj = @(u,v) dot(u,v)/dot(u,u) * u;
+
+coefs_rescaled(:,1) = coefs_rescaled(:,1) / norm(coefs_rescaled(:,1));
+
+coefs_rescaled(:,2) = coefs_rescaled(:,2) - proj( coefs_rescaled(:,1), coefs_rescaled(:,2));
+coefs_rescaled(:,2) = coefs_rescaled(:,2) / norm(coefs_rescaled(:,2));
+
+rotated_coefs = coefs_rescaled;
+for i = 2:size(coefs,2)
+    x_vec = coefs_rescaled(:,i);
+    [~,index] = max(abs(real(x_vec(1:x0.size_scalar))));
+    angle = atan( imag(x_vec(index))/real(x_vec(index)));
+    x_vec = exp( - 1i * angle) * x_vec;
+    % bringing x_Xi_vec to be symmetric (by multiplication with the appropriate complex rotation)
+    rotated_x_vec = Xi_vec2vec(symmetrise(vec2Xi_vec(x_vec,x0)));
+    if norm(rotated_x_vec - x_vec)>10^-5
+        warning('The rotation might not have been sufficient')
+    end
+    rotated_coefs(:,i) = rotated_x_vec;
+end
+
+
+% 
+% 
+% 
+% 
+% A = [imag(x), real(x), imag(y), real(y), zeros(x0.size_scalar,4);
+%     zeros(x0.size_scalar,4),imag(x), real(x), imag(y), real(y)];
+% 
+% 
+% f = @(c) [A*c;
+%     dot(c(1)+1i*c(2),c(1)+1i*c(2)) + dot(c(3)+1i*c(4),c(3)+1i*c(4)) - 1
+%     dot(c(5)+1i*c(6),c(5)+1i*c(6)) + dot(c(7)+1i*c(8),c(7)+1i*c(8)) - 1
+%     ((c(1)+1i*c(2))*x + (c(3)+1i*c(4))*y)'* ( (c(5)+1i*c(6))*x + (c(7)+1i*c(8))*y)];
+% 
+% der_perpend = @(c) [ conj(x).'*((c(5)+1i*c(6))*x + (c(7)+1i*c(8))*y) 
+%     -conj(x).'*((c(5)+1i*c(6))*x + (c(7)+1i*c(8))*y) 
+%     conj(y).'* ((c(5)+1i*c(6))*x + (c(7)+1i*c(8))*y)
+%     -conj(y).'*((c(5)+1i*c(6))*x + (c(7)+1i*c(8))*y)
+%     (((c(1)-1i*c(2))*conj(x) + (c(3)-1i*c(4))*conj(y)).' * x)
+%     (((c(1)-1i*c(2))*conj(x) + (c(3)-1i*c(4))*conj(y)).' * x)
+%     (((c(1)-1i*c(2))*conj(x) + (c(3)-1i*c(4))*conj(y)).' * y)
+%     (((c(1)-1i*c(2))*conj(x) + (c(3)-1i*c(4))*conj(y)).' * y)].';
+% 
+% df = @(c) [A
+%     2*c(1:4).', 0*c(5:8).'
+%     0*c(1:4).', 2*c(5:8).'
+%     der_perpend(c)];
+% 
+% rescaling_coefs = newton(first_rotation.', f, df);
+% 
+% coefs_rescaled = 0*coefs;
+% coefs_rescaled(:,1) = (rescaling_coefs(1)+1i*rescaling_coefs(2)) * coefs(:,1) + ...
+%      (rescaling_coefs(3)+1i*rescaling_coefs(4)) * coefs(:,2);
+% coefs_rescaled(:,2) = (rescaling_coefs(5)+1i*rescaling_coefs(6)) * coefs(:,1) + ...
+%      (rescaling_coefs(7)+1i*rescaling_coefs(8)) * coefs(:,2);
+
+end
+
+function x = newton(x0, f, df)
+x = x0;
+for i = 1:100
+    x = x - df(x)\f(x);
+    if norm(f(x))<10^-10
+        break
+    end
 end
 end
 
